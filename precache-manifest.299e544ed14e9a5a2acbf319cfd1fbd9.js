2ae2e88b8f7ac66dfc4de4b136c8220db36467da
unkn0wnNTC <joaovictorcabtalsteam@gmail.com>
2019-08-20 00:00:08 -0300

    Updates

---
+++ precache-manifest.299e544ed14e9a5a2acbf319cfd1fbd9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1192e4f1708986a764348a20733389",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f13addedc100c6ae44",
+    "url": "/static/js/2.578de4ad.chunk.js"
+  },
+  {
+    "revision": "8feaaa4fb966551d6255",
+    "url": "/static/js/main.730fb050.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
