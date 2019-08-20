8596d3abfe07aa763c13499e98def7d8677c0a85
unkn0wnNTC <joaovictorcabtalsteam@gmail.com>
2019-08-20 20:46:35 -0300

    Updates

---
+++ precache-manifest.f199664fd54ad73199baf20b6b718315.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee42082097a106ec6dcf2319713a88bb",
+    "url": "/pipefy/index.html"
+  },
+  {
+    "revision": "573746ba9a9457b4e7c8",
+    "url": "/pipefy/static/js/2.b786e5e2.chunk.js"
+  },
+  {
+    "revision": "f393bc4da93fde24ec68",
+    "url": "/pipefy/static/js/main.c44c6dc7.chunk.js"
+  },
+  {
+    "revision": "1bf5157cbc8a4126ee40",
+    "url": "/pipefy/static/js/runtime~main.16f8e946.js"
+  }
+]);
